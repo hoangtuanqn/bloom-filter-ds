fa1a2f8f45e640e83313946f0be800da9d289fe0
Hoàng Tuấn <phamhoangtuanqn@gmail.com>
2026-05-04 13:38:17 +0700

    feat: test bloom filter

--- index.ts
+++ index.ts
@@ -1,3 +1,18 @@
+import readline from "node:readline";
 import BloomFilter from "./BloomFilter";
 
-const bloomFilter = new BloomFilter();
+const bf = new BloomFilter(1_000_000_000, 0.01);
+
+// seed demo
+for (let i = 0; i < 7_000_000; i++) {
+  const email = `email${i}@gmail.com`;
+  bf.add(email);
+  //   console.log(email);
+}
+let count = 0;
+for (let i = 0; i < 7_000_000; i++) {
+  const email = `email-1@gmail.com`;
+
+  if (bf.has(email).result == "definitely_not") count++;
+}
+console.log(count);
